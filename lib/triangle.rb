13ce412cd2e1c872fcef90eca553482dc7ee5391
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-15 23:19:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,8 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(length1, length2, length3)
-    
+    equilateral = length1 == length2 && length2 == length3 && length3 == length1
+      
   end
   
 end
