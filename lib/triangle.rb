14295730f15db6fe8dbed95a3adc1122d6217631
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-15 23:51:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ class Triangle
   
   def kind 
     sides = []
+    sides.sum {
      if s1 <= 0 || s2 <= 0 || s3 <= 0 || s1 + s2 <= s3 || s2 + s3 <= s1 || s3 + s1 <= s2
       raise TriangleError
     elsif s1 == s2 && s2 == s3
